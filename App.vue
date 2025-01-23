e2620a33a97ba1335222f7d1e4034d9853478840
GSJ <2383167524@qq.com>
2025-01-23 15:28:32 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+      <router-view></router-view>
+      <meta http-equiv="refresh" content="URL=Login.vue">
+    </div>
+  </template>
+   
+  <script>
+  export default {
+    name: 'app',
+    data () {
+        return {
+        }
+    },
+    methods: {},
+  }
+  </script>
+   
+  <style>
+    .header-css{
+      height:6rem;
+      background: black;
+      color: white;
+      line-height: 6rem;
+      text-align: center;
+      font-size: 15px;
+    }
+    .header-css a {
+      color: white;
+      padding: 0 2rem;
+    }
+  </style>
---
+++ main.js
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+import App from './App.vue';
+import VueResource from 'vue-resource';
+import router from './JS/router.js';
+import './assets/css/basic.css';
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/global.css'
+import './JS/element.js'
+import axios from 'axios';
+import TreeTable from 'vue-table-with-tree-grid';
+import moment from "moment";
+import './JS/element.js'
+import VueQuillEditor from 'vue-quill-editor'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+axios.interceptors.request.use(config => {
+    console.log(config);
+    config.headers.Authorization = window.sessionStorage.getItem('token');
+    return config;
+})
+axios.interceptors.response.use(config => {
+    console.log(config);
+    return config;
+})
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false;
+Vue.use(VueResource);
+Vue.use(VueQuillEditor)
+Vue.use(Mint);
+Vue.use(ElementUI);
+Vue.component('tree-table', TreeTable);
+Vue.filter('Format', function(input, fmtstring) {
+    return moment(input).format(fmtstring);
+});
+Vue.prototype.$moment = moment;
+
+new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+})
